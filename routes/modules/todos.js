53462a7d13fec4efec2066d2371726d8366db439
Yen <p12427@gmail.com>
2022-07-30 01:52:45 +0800

    feat: add edit todo feature

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -19,9 +19,31 @@ router.post('/', (req, res) => {
 router.get('/:id', (req, res) => {
   const id = req.params.id
   Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+// get edit todo page
+router.get('/:id/edit', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  Todo.findOne({ where: { id, UserId } })
+    .then(todo => res.render('edit', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+// put edited todo
+router.put('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  const { name, isDone } = req.body
+  Todo.findOne({ where: { id, UserId } })
     .then(todo => {
-      res.render('detail', { todo: todo.toJSON() })
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
     })
+    .then(() => res.redirect(`/todos/${id}`))
     .catch(error => console.log(error))
 })
 
